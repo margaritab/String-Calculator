8500efd9e2759fe637ea9cda3b6948c219de89a8
margarita braun <margaritabraun@gmail.com>
2013-12-10 20:41:06 +0200

    16th commit - function for supporting different delimiters - green

--- String Calculator/src/string/calculator/tdd/stringCalculator.java
+++ String Calculator/src/string/calculator/tdd/stringCalculator.java
@@ -15,15 +15,49 @@ public class stringCalculator {
 
 	private int handleMultipalNumbers(String str)
 	{
-		String[] strings = str.split(",|\n");
+		/*String delimiter = ",|\n";
+		
+		String[] strings = str.split(delimiter);
 		int num = 0;
 		
 		for(int i=0; i<strings.length; i++)
 		{
 			num+= Integer.parseInt(strings[i]);
 		}
+		return num;*/
+		
+		String delimiter = ",|\n";
+		
+		String[] strings = str.split(delimiter);
+		char[] arr;
+		
+		int num = 0;
+		
+		if(strings[0].contains("//"))
+		{
+			arr = strings[0].toCharArray();
+			delimiter += "|" + arr[2];
+
+			for(int i=1; i<strings.length; i++)
+			{
+				String[] stringDeli = strings[i].split(delimiter);
+				
+				for(int j=0; j<stringDeli.length;j++)
+					num+= Integer.parseInt(stringDeli[j]);
+			}
+		}
+		else
+		{
+			for(int i=0; i<strings.length; i++)
+			{
+				num+= Integer.parseInt(strings[i]);
+			}
+		}
+		
 		return num;
 	}
 	
+	
+	
+	
 }
-
